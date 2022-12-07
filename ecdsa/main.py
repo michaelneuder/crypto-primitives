770bb7c45b3daeb7d99a86b5d6ca2cb3cc15ad3f
Michael Neuder <mikeneuder@google.com>
2022-12-06 19:26:15 -0500

    ecdsa python

---
+++ ecdsa/main.py
@@ -0,0 +1,41 @@
+from json import dumps
+from ellipticcurve.ecdsa import Ecdsa
+from ellipticcurve.privateKey import PrivateKey
+
+
+# Generate privateKey from PEM string
+privateKey = PrivateKey.fromPem("""
+    -----BEGIN EC PARAMETERS-----
+    BgUrgQQACg==
+    -----END EC PARAMETERS-----
+    -----BEGIN EC PRIVATE KEY-----
+    MHQCAQEEIODvZuS34wFbt0X53+P5EnSj6tMjfVK01dD1dgDH02RzoAcGBSuBBAAK
+    oUQDQgAE/nvHu/SQQaos9TUljQsUuKI15Zr5SabPrbwtbfT/408rkVVzq8vAisbB
+    RmpeRREXj5aog/Mq8RrdYy75W9q/Ig==
+    -----END EC PRIVATE KEY-----
+""")
+
+# Create message from json
+message = dumps({
+    "transfers": [
+        {
+            "amount": 100000000,
+            "taxId": "594.739.480-42",
+            "name": "Daenerys Targaryen Stormborn",
+            "bankCode": "341",
+            "branchCode": "2201",
+            "accountNumber": "76543-8",
+            "tags": ["daenerys", "targaryen", "transfer-1-external-id"]
+        }
+    ]
+})
+
+signature = Ecdsa.sign(message, privateKey)
+
+# Generate Signature in base64. This result can be sent to Stark Bank in the request header as the Digital-Signature parameter.
+print(signature.toBase64())
+
+# To double check if the message matches the signature, do this:
+publicKey = privateKey.publicKey()
+
+print(Ecdsa.verify(message, signature, publicKey))
